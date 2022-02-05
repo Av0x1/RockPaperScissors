ae9bccb17a9a31d50b48b356dfd356875a967ebe
seeker1366@gmail.com <86572524+Av0x1@users.noreply.github.com>
2022-02-05 16:00:20 +0100

    Add project files.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			UserManager userManager = new UserManager();
+			Random random = new Random();
+
+			Console.WriteLine("Guten Tag, dies ist ein Schere-Stein-Papier Spiel. Bitte wählen Sie Ihren Gegenstand:\nFür Schere bitte 'S' drücken.\nFür Stein bitte 'R' drücken.\nFür Papier bitte 'R' drücken.");
+
+			String tool = userManager.ReadInput();
+
+			String[] botTools = new String[3] { "s", "r", "p" };
+
+			Int32 bot = random.Next(0, 3);
+
+			//Schere
+			if(tool == "s")
+			{
+				userManager.DrawChecker(tool, botTools[bot]);
+
+				if()
+			}
+			//Stein
+			else if(tool == "r")
+			{
+				userManager.DrawChecker(tool, botTools[bot]);
+
+			}
+			//Papier
+			else
+			{
+				userManager.DrawChecker(tool, botTools[bot]);
+
+			}
+		}
+	}
+}
---
+++ RockPaperScissors/UserManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+	public class UserManager
+	{
+		public String ReadInput()
+		{
+			while(true)
+			{
+				String input = Console.ReadLine();
+
+				if(!input.ToLower().Equals("s") && !input.ToLower().Equals("r") && !input.ToLower().Equals("p"))
+				{
+					Console.WriteLine("Ungültige Eingabe!");
+				}
+				else
+				{
+					return input;
+				}
+			}
+		}
+
+		public void DrawChecker(String user, String bot)
+		{
+			if(user.Equals(bot))
+			{
+				Console.WriteLine("Spiel ist unentschieden.");
+			}
+		}
+	}
+}
